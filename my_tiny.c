0a2789f777734a44d676e0f53892e19231cdc45b
Ray <ray1997ray75@gmail.com>
2025-08-25 13:15:16 +0800

    first commit

---
+++ my_tiny.c
@@ -0,0 +1,43 @@
+#include <arpa/inet.h>          /* inet_ntoa */
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#define LISTENQ  1024  /* second argument to listen() */
+#define MAXLINE 1024   /* max length of a line */
+#define RIO_BUFSIZE 1024
+
+
+typedef struct {
+    int rio_fd;                 /* descriptor for this buf */
+    int rio_cnt;                /* unread byte in this buf */
+    char *rio_bufptr;           /* next unread byte in this buf */
+    char rio_buf[RIO_BUFSIZE];  /* internal buffer */
+} rio_t;
+
+/* Simplifies calls to bind(), connect(), and accept() */
+typedef struct sockaddr SA;
+
+typedef struct {
+    char filename[512];
+    off_t offset;              /* for support Range */
+    size_t end;
+} http_request;
+
+typedef struct {
+    const char *extension;
+    const char *mime_type;
+} mime_map;
